b1c60d9bd7c0f0013ad82507e6667fb9a897b43c
David Zsombok <zsombokd@gmail.com>
2022-03-05 23:51:52 +0100

    Reformat code

--- src/main/java/com/emarsys/Homework/HomeworkApplication.java
+++ src/main/java/com/emarsys/Homework/HomeworkApplication.java
@@ -1,5 +1,6 @@
 package com.emarsys.Homework;
 
+import com.emarsys.Homework.exceptions.OutOfWorkingHoursException;
 import com.emarsys.Homework.utilities.DateCalculator;
 import java.time.LocalDateTime;
 import org.springframework.boot.CommandLineRunner;
@@ -9,19 +10,23 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class HomeworkApplication implements CommandLineRunner {
 
-	private final DateCalculator dateCalculator;
+  private final DateCalculator dateCalculator;
 
-	public HomeworkApplication(DateCalculator dateCalculator) {
-		this.dateCalculator = dateCalculator;
-	}
+  public HomeworkApplication(DateCalculator dateCalculator) {
+    this.dateCalculator = dateCalculator;
+  }
 
-	public static void main(String[] args) {
-		SpringApplication.run(HomeworkApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(HomeworkApplication.class, args);
+  }
 
-	@Override
-	public void run(String... args) throws Exception {
-		dateCalculator.CalculateDueDate(LocalDateTime.parse("2022-03-02T10:17"
-), 17);
-	}
+  @Override
+  public void run(String... args) throws Exception {
+    try {
+      LocalDateTime result = dateCalculator.CalculateDueDate(LocalDateTime.parse("2022-03-04T17:00"), 11);
+      System.out.println(result);
+    } catch (OutOfWorkingHoursException e) {
+      System.err.println(e.getMessage());
+    }
+  }
 }
