892d53e659af0522273ebc835986d43af4cfdb81
David Zsombok <zsombokd@gmail.com>
2022-03-05 20:47:07 +0100

    Init commit

---
+++ src/main/java/com/emarsys/Homework/utilities/DateCalculator.java
@@ -0,0 +1,11 @@
+package com.emarsys.Homework.utilities;
+
+import java.time.LocalDateTime;
+
+public class DateCalculator {
+
+    public static LocalDateTime CalculateDueDate(LocalDateTime submitDate, int turnaroundTime) {
+
+    }
+
+}
---
+++ src/test/java/com/emarsys/Homework/utilities/DateCalculatorTest.java
@@ -0,0 +1,17 @@
+package com.emarsys.Homework.utilities;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class DateCalculatorTest {
+
+  @BeforeEach
+  void setUp() {
+  }
+
+  @Test
+  void calculateDueDate() {
+  }
+}
